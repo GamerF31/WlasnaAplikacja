b1de390e1ca80e6998dd662b97a0a049141f62b7
GamerF31 <przemekhubacz@interia.pl>
2024-03-14 16:17:20 +0100

    zmianytest

---
+++ ConsoleApp1/CompetitorTests/UnitTest1.cs
@@ -0,0 +1,29 @@
+using ConsoleApp1;
+
+namespace CompetitorTests
+{
+    public class Tests
+    {
+        
+        [Test]
+        public void Test1()
+        {
+            // arrange
+            
+            var competitor = new LongJumpCompetitorsInFile("Lionel", "Messi");
+            competitor.AddGrade(5);
+            competitor.AddGrade(6);
+            competitor.AddGrade(4);
+            competitor.AddGrade(3);
+            competitor.AddGrade(2);
+
+            // act
+            var result = competitor.GetStatistics();
+
+            // assert
+            Assert.AreEqual(4, result.Average);
+            Assert.AreEqual(6, result.Max);
+            Assert.AreEqual(2, result.Min);
+        }
+    }
+}
